1e09eff5358d0344a5c2ae3ccd8a700cf6abc117
jason-mcfeeley <programmer@hs-lab-8.local>
2015-10-23 08:26:44 -0400

    tabs/spacing/tests all fixed

--- hangman.py
+++ hangman.py
@@ -2,7 +2,7 @@
 
 
 def Main():
-    Get_word
+    word = Get_Word()
     while (end == false):
         check_letter (Jack)
         Print_Right_or_Wrong 
@@ -13,10 +13,9 @@ def Main():
 
 
 
-Def Get_Word()
+Def Get_Word():
     words = [‘dog’, ’cat’, ’wolf’, ‘fox’, ‘rabbit’, ‘parrot’]
-            print(random.choice(words))
-return 
+            return(random.choice(words))
 
     
 Def Check_Letter(word, word_with_blanks, guesses, correct_guesses ):
@@ -31,7 +30,7 @@ Def Check_Letter(word, word_with_blanks, guesses, correct_guesses ):
 Def Right_or_wrong(guess)
     if guess == 1:
         print("You Guessed Right!")
-else:
+    else:
         print("Oh No! You Guessed wrong")
 
 Def Draw_Man(guesses, correct_guesses):
@@ -108,33 +107,33 @@ Def write_guesses(guesses):
 
 
 Def Check_End(word, word_with_blanks, guesses, correct_guesses):
-x = guesses-correct_guesses
-if x == 7:
-print(“Game over! You Lose!”)
-!!!!!! - Some Sort Of End Thing - !!!!!!!
+    x = guesses-correct_guesses
+    if x == 7:
+        print(“Game over! You Lose!”)
+        #!!!!!! - Some Sort Of End Thing - !!!!!!!
     elif word == word_with_blanks:
         print(“You Win! Congrats!”)
     else:
         print(“Keep Playing”)
 
+######TESTS#####
 
-TESTS
 def Test_Get_Word():
     print(“test word 1”)
-word = Get_Word()
-print(word)
-print(“test word 2”)
-Get_Word()
-print(word) # FIXME
-print(“test word 3”)
-Get_Word()
-print(word)
+    word = Get_Word()
+    print(word)
+    print(“test word 2”)
+    word = Get_Word()
+    print(word) 
+    print(“test word 3”)
+    word = Get_Word()
+    print(word)
 
 
-def Test_Check_Letter(Word, word with blanks, correct_guesses, guesses):
+def Test_Check_Letter():
     Check_Letter(“House”, [“H__S_”],[”H”,”S”],[“H”,”S”,”G”,”Q”])
-Check_Letter(“Computer”,“_o____er”,[“O”,”E”,”R”],[“E”,”S”,”E”,”Q”,”R”])
-Check_Letter(“Sweater”,“_we___r”,[“W”,”E”,”R”],[“W”,”Q”,”E”,”R”])
+    Check_Letter(“Computer”,“_o____er”,[“O”,”E”,”R”],[“E”,”S”,”E”,”Q”,”R”])
+    Check_Letter(“Sweater”,“_we___r”,[“W”,”E”,”R”],[“W”,”Q”,”E”,”R”])
 
 
 def Test_Print_Right_Or_Wrong():
@@ -142,14 +141,14 @@ def Test_Print_Right_Or_Wrong():
     Print_Right_Or_Wrong(1)
 
 def test_draw_man():
-draw_man(1,1)
-draw_man(1,0)
-draw_man(4,2)
-draw_man(6,3)
-draw_man(9,5)
-draw_man(8,3)
-draw_man(8,2)
-draw_man(7,0)
+    draw_man(1,1)
+    draw_man(1,0)
+    draw_man(4,2)
+    draw_man(6,3)
+    draw_man(9,5)
+    draw_man(8,3)
+    draw_man(8,2)
+    draw_man(7,0)
 
 def Test_Write_Guesses():
     Write_Guesses([“B”,”H”,”O”,”W”])
@@ -157,8 +156,8 @@ def Test_Write_Guesses():
 
 def Test_Write_Word_Guess():
     Write_Word_Guess(“B_t”)
-Write_Word_Guess(“_om_u__r”)
-Write_Word_Guess(“O__n_e”)
+    Write_Word_Guess(“_om_u__r”)
+    Write_Word_Guess(“O__n_e”)
 
 def Test_Check_End():
     Check_End(“House”,“H__S_”],[”H”,”S”],[“H”,”S”,”G”,”Q”])
