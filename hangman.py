3693305bf5f0dd24b551f9bd06016f94b8c2d0a6
Programmer Account <programmer@hs-lab-84.local>
2015-10-26 12:54:20 -0400

    fixed everything

--- hangman.py
+++ hangman.py
@@ -3,45 +3,51 @@
 
 def Main():
     word = Get_Word()
-    while (end == false):
-        Check_Letter (word, word_with_blanks, guesses, correct_guesses Jack)
-        Print_Right_or_Wrong 
-        Draw_Man (Jason)
-        Write_Guesses
-        Write_Word_Guess
-        Check_end
+    end = False
+    guesses = []
+    correct_guesses = []
+    while (end == False):
+        word_with_blanks,correct = Check_Letter (
+            word, word_with_blanks, guesses, correct_guesses
+            )
+        Draw_Man (guesses, correct_guesses)
+        Write_Guesses(guesses)
+        Write_Word_Guess(word_with_blanks)
+        end = Check_end(word,word_with_blanks, guesses, correct_guesses)
 
-
-
-Def Get_Word():
-    words = [‘dog’, ’cat’, ’wolf’, ‘fox’, ‘rabbit’, ‘parrot’]
-            return(random.choice(words))
+def Get_Word():
+    words = ['dog', 'cat', 'wolf', 'fox', 'rabbit', 'parrot']
+    return(random.choice(words))
 
     
-Def Check_Letter(word, word_with_blanks, guesses, correct_guesses ):
-    guess = raw_input(‘Guess a letter’)
-    if guess in word:       # from stack exchange 
-        print "you are right"
-    if guess not in word:
-        print "guess again"
+def Check_Letter(word, word_with_blanks, guesses, correct_guesses ):
+    '''Asks user for input. Returns updated word list and if player is right or wrong
+
+    e.g. if the word='cat' and the guess is 'a', it returns
+    ["_A_",True] 
+    and if the guess were 'o' it would return...
+    ['___',False]
+    '''
+    guess = raw_input('Guess a letter')
     if guess in word:
         search = guess
-        x =  sentence.find(search)
+        x = word.find(search)
         s = word
         l = list(s)
         l[x] = guess
         news = "".join(l)
-        print news
-        
+        print news, "You're right!"
+    if guess not in word:
+        print "guess again"
+ 
 
-
-Def Right_or_wrong(guess)
+def Right_or_wrong(guess):
     if guess == 1:
         print("You Guessed Right!")
     else:
         print("Oh No! You Guessed wrong")
 
-Def Draw_Man(guesses, correct_guesses):
+def Draw_Man(guesses, correct_guesses):
     x = guesses-correct_guesses
     if x == 0:
         print(x," wrong,", 7-x, "lives left")
@@ -108,40 +114,40 @@ Def Draw_Man(guesses, correct_guesses):
         print("  I        I")
         print("__I__     / \ ")
 
-Def write_guesses(guesses):
+def write_guesses(guesses):
     print("Your Guesses:", guesses)
 
 
 
 
-Def Check_End(word, word_with_blanks, guesses, correct_guesses):
+def Check_End(word, word_with_blanks, guesses, correct_guesses):
     x = guesses-correct_guesses
     if x == 7:
-        print(“Game over! You Lose!”)
+        print("Game over! You Lose!")
         #!!!!!! - Some Sort Of End Thing - !!!!!!!
     elif word == word_with_blanks:
-        print(“You Win! Congrats!”)
+        print("You Win! Congrats!")
     else:
-        print(“Keep Playing”)
+        print("Keep Playing")
 
 ######TESTS#####
 
 def Test_Get_Word():
-    print(“test word 1”)
+    print("test word 1")
     word = Get_Word()
     print(word)
-    print(“test word 2”)
+    print("test word 2")
     word = Get_Word()
     print(word) 
-    print(“test word 3”)
+    print("test word 3")
     word = Get_Word()
     print(word)
 
 
 def Test_Check_Letter():
-    Check_Letter(“House”, [“H__S_”],[”H”,”S”],[“H”,”S”,”G”,”Q”])
-    Check_Letter(“Computer”,“_o____er”,[“O”,”E”,”R”],[“E”,”S”,”E”,”Q”,”R”])
-    Check_Letter(“Sweater”,“_we___r”,[“W”,”E”,”R”],[“W”,”Q”,”E”,”R”])
+    Check_Letter("House", "H__S_",["H","S"],["H","S","G","Q"])
+    Check_Letter("Computer","_o____er",["O","E","R"],["E","S","E","Q","R"])
+    Check_Letter("Sweater","_we___r",["W","E","R"],["W","Q","E","R"])
 
 
 def Test_Print_Right_Or_Wrong():
@@ -159,15 +165,16 @@ def test_draw_man():
     draw_man(7,0)
 
 def Test_Write_Guesses():
-    Write_Guesses([“B”,”H”,”O”,”W”])
-    Write_Guesses([“P”,”K”,”A”,”R”,”E”])
+    Write_Guesses(["B","H","O","W"])
+    Write_Guesses(["P","K","A","R","E"])
 
 def Test_Write_Word_Guess():
-    Write_Word_Guess(“B_t”)
-    Write_Word_Guess(“_om_u__r”)
-    Write_Word_Guess(“O__n_e”)
+    Write_Word_Guess("B_t")
+    Write_Word_Guess("_om_u__r")
+    Write_Word_Guess("O__n_e")
 
 def Test_Check_End():
-    Check_End(“House”,“H__S_”],[”H”,”S”],[“H”,”S”,”G”,”Q”])
-    Check_End(“Computer”,”_o____er”,[“O”,”E”,”R”],[“E”,”S”,”E”,”Q”,”R”])
+    Check_End("House","H__S_",["H","S"],["H","S","G","Q"])
+    Check_End("Computer","_o____er",["O","E","R"],["E","S","E","Q","R"])
 
+Test_Check_Letter()
